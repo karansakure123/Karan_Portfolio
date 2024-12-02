537016e8dc22172d9f69d2fbb894d120ca6dddbc
Karan Sakure <148356294+karansakure123@users.noreply.github.com>
2024-12-02 22:21:57 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -14,7 +14,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/karansakure123?tab=repositories",
   resume:
-    "https://drive.google.com/file/d/1rI1ym1fv7IWQhy4m02px6nD9eulxbEwS/view",
+    "https://drive.google.com/file/d/1ZOUkjxsPMnTT7TYqj3TbLbeQQ-ZUdXjR/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/karansakure",
   twitter:"https://x.com/a37113363?t=SlNUV9R-qZ4w8920pjzmhw&s=09",
   insta: "https://www.instagram.com/_karansakure",
